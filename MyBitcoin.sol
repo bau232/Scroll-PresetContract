57e189948edea64fd6c82292ae1c39eddf8039f0
Nguyễn Văn Đài <59951124+nguyenvandai995@users.noreply.github.com>
2023-10-27 18:36:00 +0700

    Update MyBitcoin.sol

--- MyBitcoin.sol
+++ MyBitcoin.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MztacatCommunity {
+contract Mybitcoin {
     string public name = "Mybitcoin";
     string public symbol = "MBTC";
     uint8 public decimals = 18;
